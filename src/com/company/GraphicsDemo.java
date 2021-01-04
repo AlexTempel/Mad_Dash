745979c57d55cad56346c9be261635acf4b6d349
Alex <alex.dirk.tempel@gmail.com>
2021-01-04 17:32:26 -0500

    Added Position Variables

--- src/com/company/GraphicsDemo.java
+++ src/com/company/GraphicsDemo.java
@@ -10,7 +10,12 @@ import java.awt.event.KeyListener;
 public class GraphicsDemo extends JPanel implements KeyListener /* To get Keyboard Input*/, ActionListener {
 
     Timer timer = new Timer(1000,this); //Constructing a Timer
+    //Variables
     int seconds = 0; //Variable for Time
+    int playerx = 100, playery = 500; //Variables for the player's position
+    boolean space = false; //Variable to see if the space bar has been pressed
+    int playersteps = 0; //Variable to count how many steps you've taken
+    boolean flat = false, top = false; //Variables to see what part of the track you are on
 
     public GraphicsDemo() {
         addKeyListener(this);
@@ -46,6 +51,10 @@ public class GraphicsDemo extends JPanel implements KeyListener /* To get Keyboa
         g2D.drawArc(975,100,750,800,90,-180);
         g2D.drawArc(1050,175,600,650,90,-180);
 
+        //Creating the Runner
+        g2D.setColor(Color.YELLOW);
+        g2D.fillOval(playerx,playery,30,30);
+
         //Creating the Clock on Screen
         g2D.setColor(Color.BLACK);
         g2D.setFont(new Font("Comic Sans",Font.PLAIN,75));
@@ -59,12 +68,26 @@ public class GraphicsDemo extends JPanel implements KeyListener /* To get Keyboa
         seconds++;
         System.out.println(seconds + " seconds have passed");
 
+        space = false;
+        if (playerx > 450 && playerx < 1350) {
+            flat = true;
+        } else {
+            flat = false;
+        }
+        if (playery > 500) {
+            top = true;
+        } else {
+            top = false;
+        }
+
         repaint();
     }
 
     //Keyboard Input
     @Override
     public void keyTyped(KeyEvent e) {
+        space = true;
+        playersteps++;
     }
     @Override
     public void keyPressed(KeyEvent e) {
